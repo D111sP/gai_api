7a62f99a4fe0ef54b983a65e5a31eb1119e14c20
D111sP <115263492+D111sP@users.noreply.github.com>
2023-01-02 10:57:35 +0500

    инпут для майна

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def get_info_GIBDD(vin):
 if __name__ == '__main__':
     model = keras.models.load_model('model/model_80_30_loss_0_11_acc_0_97.h5')
     #vin = "XWEDH511AB0010069"
-    vin = get_vin("Е790АС186")
+    vin = get_vin(str(input("Ведите номер: ")))
     time_start = datetime.datetime.now()
     get_info_GIBDD(vin)
     print(f"Time requiers: {datetime.datetime.now() - time_start}")
